82bd32793310c039716e14d4be5f9092445417c9
zach.chen <zach.chen@thinkpower-info.com>
2020-11-26 12:24:16 +0800

    update api

--- src/components/Bill/Bill.components.jsx
+++ src/components/Bill/Bill.components.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BillContainer, Bill, BillTitle, BillWrapper, BillHr, BillButton, BillInput } from './Bill.style'
 import { withRouter } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import Button from '../Button/Button.components'
+import POST_API from '../../api/default'
+import example from '../../util/example'
 export default withRouter(({ location, match, history }) => {
+    const dispatch = useDispatch()
+
     const [student, setStudent] = useState(location.state ? location.state.student : {})
     const [bill, setBill] = useState(location.state ? location.state.student.paymentList[location.state.student.paymentList.length - 1] : {})
-    const [temp, setTemp] = useState({ name: '', expense: '', remark: '', expenseSmonth: '',expenseEmonth: '' })
+    const [temp, setTemp] = useState({ name: '', expense: '', remark: '', expenseSmonth: '', expenseEmonth: '' })
     const { mode } = match.params
+
+    useEffect(() => {
+        if (location.state.student) { // find recent bill
+            let g = location.state.student.grade === '全部' ? '' : example.eduLevel.find(edu => edu.title == location.state.student.grade).id
+            POST_API('/academy03/03', { grade: g, stdntId: location.state.student.stdntId }).then(result => {
+                //console.log(result)
+                dispatch({ type: 'SEARCH_RECENT_BILL', payload: { paymentList: [] } })
+            })
+        }
+    }, [location.state.student])
+
     const handleClick = (mode, i) => {
         if (mode === 'edit') {
             bill.lessonList[i].state = 'edit'
@@ -14,16 +30,16 @@ export default withRouter(({ location, match, history }) => {
         }
         else if (mode === 'add') {
             bill.lessonList.push({ id: bill.lessonList.length + 1, name: '新課程', remark: '123', expense: 100, expenseSmonth: bill.lessonList[0].expenseSmonth, expenseEmonth: bill.lessonList[0].expenseSmonth })
-            setTemp({ name: '', expense: '', remark: '', expenseSmonth: '', expenseEmonth:'' })
+            setTemp({ name: '', expense: '', remark: '', expenseSmonth: '', expenseEmonth: '' })
         }
         else if (mode === 'del') {
             bill.lessonList = bill.lessonList.filter((x, index) => index !== i)
-            setTemp({ name: '', expense: '', remark: '', expenseSmonth: '', expenseEmonth:'' })
+            setTemp({ name: '', expense: '', remark: '', expenseSmonth: '', expenseEmonth: '' })
         }
         else {
             bill.lessonList[i] = temp
             bill.lessonList[i].state = 'save'
-            setTemp({ name: '', expense: '', remark: '', expenseSmonth: '', expenseEmonth:'' })
+            setTemp({ name: '', expense: '', remark: '', expenseSmonth: '', expenseEmonth: '' })
         }
 
         setBill(bill)
@@ -31,6 +47,17 @@ export default withRouter(({ location, match, history }) => {
 
     const handleOnChange = (name, value) => {
         setTemp({ ...temp, [name]: value })
+    }
+
+    const handleSave = () => {
+        if (student) {
+            let g = student.grade === '全部' ? '' : example.eduLevel.find(edu => edu.title == student.grade).id
+            POST_API('/academy03/04', { courseFeeList: [], stdntId: student.stdntId, grade: g, paymentMonth: '10' }).then(result => {
+                console.log(result)
+                dispatch({ type: 'ADD_BILL', payload: {} })
+            })
+            // history.goBack()
+        }
     }
 
     if (mode === 'add')
@@ -65,6 +92,7 @@ export default withRouter(({ location, match, history }) => {
                     <Bill style={{ gridColumn: '4/4' }}>
                         {bill.lessonList.map(function (obj) { return parseInt(obj.expense) }).reduce((a, b) => (a) + (b), 0)}</Bill>
                 </BillWrapper>
+                <Button title={'儲存'} handleSubmit={() => { handleSave() }} />
                 <Button title={'取消'} handleOnClick={() => { history.goBack() }} />
 
             </BillContainer>
